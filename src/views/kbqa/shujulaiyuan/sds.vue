e99a5a8ccdbcb50d4fbbe4cba814a0824fa79b3d
Royal-XPS <824259085@qq.com>
2020-08-22 17:04:15 +0800

    增加模型训练的界面

--- src/views/kbqa/shujulaiyuan/sds.vue
+++ src/views/kbqa/shujulaiyuan/sds.vue
@@ -1,30 +1,29 @@
 <template>
   <div style="padding: 20px;">
     <el-form :inline="true" :model="formInline" class="demo-form-inline">
-      <el-form-item label="实体概念">
-        <el-select v-model="formInline.region" placeholder="实体概念">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="实体">
-        <el-input v-model="formInline.user" placeholder="实体"></el-input>
+      <el-form-item label="关键字">
+        <el-input v-model="formInline.user" placeholder="关键字"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
+        <el-button type="primary" @click="onSubmit">搜索</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">
+          上传SDS
+          <i class="el-icon-upload el-icon--right"></i>
+        </el-button>
       </el-form-item>
     </el-form>
 
     <el-table :data="tableData" border style="width: 100%">
-      <el-table-column fixed prop="ID" label="编号" width="150"></el-table-column>
-      <el-table-column prop="name" label="实体名" width="120"></el-table-column>
-      <el-table-column prop="province" label="省份"></el-table-column>
-      <el-table-column prop="city" label="市区"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
-      <el-table-column prop="zip" label="邮编"></el-table-column>
+      <el-table-column fixed prop="ID" label="编号" width="120"></el-table-column>
+      <el-table-column prop="filename" label="文件名" width="200"></el-table-column>
+      <el-table-column prop="size" label="文件大小" width="120"></el-table-column>
+      <el-table-column prop="abstract" label="文档摘要"></el-table-column>
       <el-table-column fixed="right" label="操作">
         <template slot-scope="scope">
-          <el-button type="primary" size="small" @click="handleClick(scope.row)">编辑</el-button>
+          <el-button type="success" size="small">下载</el-button>
+          <el-button type="primary" size="small" @click="handleClick(scope.row)">解析</el-button>
           <el-button type="danger" size="small">删除</el-button>
         </template>
       </el-table-column>
@@ -42,33 +41,45 @@ export default {
         region: ''
       },
       tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
+        ID: '10001',
+        filename: '32067丙醛.doc',
+        size: '37KB',
+        abstract: '中文名:   丙醛; 正丙醛 主要成分:   含量≥95％。危险性类别:    第3.2类  中闪点易燃液体'
       }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1517 弄',
-        zip: 200333
+        ID: '10002',
+        filename: '32073   2-丁酮.doc',
+        size: '37KB',
+        abstract: '中文名:   2-丁酮; 甲基乙基酮 英文名:   2-butanone; methyl ethyl ketone'
       }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1519 弄',
-        zip: 200333
+        ID: '10003',
+        filename: '32061无水乙醇.doc',
+        size: '28KB',
+        abstract: '无水乙醇 无水乙醇相对分子质量  46.07健康危害  本品为中枢神经系统抑制刑，首先引起兴奋，随后抑制'
       }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1516 弄',
-        zip: 200333
+        ID: '10004',
+        filename: '32162丙烯腈.doc',
+        size: '39KB',
+        abstract: '32162丙烯腈(抑制了的)  32162丙烯腈(抑制了的)  主要成分:   含量: 一级≥99％;二级 98％。'
+      }, {
+        ID: '10005',
+        filename: '32159乙腈.doc',
+        size: '35KB',
+        abstract: '第一部分  化学品及企业标识 中文名:   乙腈; 甲基氰  英文名:   acetonitrile; methyl cyanide'
+      }, {
+        ID: '10006',
+        filename: '32164异氰酸甲酯.doc',
+        size: '24KB',
+        abstract: '异氰酸甲酯; 甲基异氰酸酯; Methyl isocyanate; Isocyanatomethane; MIC; CAS：624-83-9'
+      }, {
+        ID: '10007',
+        filename: '32172正丁胺.doc',
+        size: '37KB',
+        abstract: '第一部分  化学品及企业标识  第一部分  化学品及企业标识 英文名:   butylamine; 1-aminobutane'
+      }, {
+        ID: '10008',
+        filename: '51043次氯酸钙.doc',
+        size: '25KB',
+        abstract: '中文名  次氯酸钙；漂白粉;漂粉精 英文名  calciumhypochlorite；Bleachingpowder'
       }]
     }
   },
--- src/views/kbqa/training/index.vue
+++ src/views/kbqa/training/index.vue
@@ -1,9 +1,242 @@
 <template>
-  <div></div>
+  <div style="padding:20px;">
+    <el-tabs v-model="activeName">
+      <el-tab-pane label="超参设置" name="first">
+        <el-button type="primary" @click="dialogFormVisible = true">新建超参配置</el-button>
+        <el-table :data="tableData" border style="width: 100%;margin-top:20px;">
+          <el-table-column fixed prop="ID" label="编号" width="120"></el-table-column>
+          <el-table-column prop="name" label="配置名" width="200"></el-table-column>
+          <el-table-column prop="lr" label="学习率" width="120"></el-table-column>
+          <el-table-column prop="epoch" label="迭代次数"></el-table-column>
+          <el-table-column prop="batch" label="批量大小"></el-table-column>
+          <el-table-column prop="window" label="窗口大小"></el-table-column>
+          <el-table-column fixed="right" label="操作">
+            <template slot-scope="scope">
+              <el-button type="primary" size="small" @click="handleClick(scope.row)">编辑</el-button>
+              <el-button type="danger" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-dialog title="新建超参数配置" :visible.sync="dialogFormVisible">
+          <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="配置名称">
+              <el-input v-model="form.name" placeholder="用于记录本次超参设置"></el-input>
+            </el-form-item>
+            <el-form-item label="全局配置">
+              <el-switch v-model="form.delivery"></el-switch>
+            </el-form-item>
+            <el-form-item label="学习率">
+              <el-input v-model="form.name" placeholder="lr"></el-input>
+            </el-form-item>
+            <el-form-item label="迭代次数">
+              <el-input v-model="form.name" placeholder="epoch"></el-input>
+            </el-form-item>
+            <el-form-item label="训练批次">
+              <el-input v-model="form.name" placeholder="batch-size"></el-input>
+            </el-form-item>
+            <el-form-item label="可编辑">
+              <el-radio-group v-model="form.resource">
+                <el-radio label="是"></el-radio>
+                <el-radio label="否"></el-radio>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="备注消息">
+              <el-input v-model="form.desc" type="textarea"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">立即创建</el-button>
+              <el-button>取消</el-button>
+            </el-form-item>
+          </el-form>
+        </el-dialog>
+      </el-tab-pane>
+      <el-tab-pane label="历史模型" name="second">
+        <el-table :data="tableData2" border style="width: 100%;margin-top:20px;">
+          <el-table-column fixed prop="ID" label="编号" width="120"></el-table-column>
+          <el-table-column prop="name" label="配置名" width="200"></el-table-column>
+          <el-table-column prop="train" label="训练集最高准确度"></el-table-column>
+          <el-table-column prop="test" label="测试集最高准确度"></el-table-column>
+          <el-table-column prop="val" label="验证集最高准确度"></el-table-column>
+          <el-table-column prop="time" label="训练时长"></el-table-column>
+          <el-table-column fixed="right" label="操作">
+            <template slot-scope="scope">
+              <el-button type="primary" size="small" @click="handleClick(scope.row)">编辑</el-button>
+              <el-button type="danger" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="模型测试" name="third">
+        <el-form :inline="true" class="demo-form-inline">
+          <el-form-item label="待测模型">
+            <el-select placeholder="请选择待测模型">
+              <el-option label="模型100001" value="shanghai"></el-option>
+              <el-option label="模型100002" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="测试数据">
+            <el-input placeholder="Data"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary">
+              上传数据
+              <i class="el-icon-upload el-icon--right"></i>
+            </el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary">开始测试</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+      <el-tab-pane label="全局设置" name="fourth">
+        <el-form :inline="true" class="demo-form-inline">
+          <el-form-item label="模型">
+            <el-select placeholder="请选择训练模型">
+              <el-option label="模型100001" value="shanghai"></el-option>
+              <el-option label="模型100002" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary">设为全局模型</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Training'
+  name: 'Training',
+  data() {
+    return {
+      activeName: 'first',
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '是',
+        desc: ''
+      },
+      tableData: [{
+        ID: '10001',
+        name: '2020第一次测试配置',
+        lr: '0.001',
+        epoch: 2000,
+        batch: 2,
+        window: 3
+      }, {
+        ID: '10002',
+        name: 'H算法配置',
+        lr: '0.002',
+        epoch: 50000,
+        batch: 4,
+        window: 3
+      }, {
+        ID: '10003',
+        name: 'Test Config',
+        lr: '0.001',
+        epoch: 1000,
+        batch: 2,
+        window: 3
+      }, {
+        ID: '10004',
+        name: 'TextCNN',
+        lr: '0.001',
+        epoch: 100,
+        batch: 2,
+        window: 2
+      }, {
+        ID: '10005',
+        name: 'RNN Attention',
+        lr: '0.001',
+        epoch: 2000,
+        batch: 2,
+        window: 3
+      }, {
+        ID: '10006',
+        name: '综合模型超参',
+        lr: '0.001',
+        epoch: 1000,
+        batch: 2,
+        window: 3
+      }, {
+        ID: '10007',
+        name: '正式上线前配置测试',
+        lr: '0.001',
+        epoch: 200,
+        batch: 2,
+        window: 3
+      }, {
+        ID: '10008',
+        name: '正式环境配置',
+        lr: '0.001',
+        epoch: 500,
+        batch: 2,
+        window: 3
+      }],
+      tableData2: [{
+        ID: '10001',
+        name: '2020第一次测试配置',
+        train: '98%',
+        test: '83%',
+        val: '48%',
+        time: '2.5h'
+      }, {
+        ID: '10002',
+        name: 'H算法配置',
+        train: '98%',
+        test: '83%',
+        val: '48%',
+        time: '1.5h'
+      }, {
+        ID: '10003',
+        name: 'Test Config',
+        train: '98%',
+        test: '83%',
+        val: '48%',
+        time: '2.5h'
+      }, {
+        ID: '10004',
+        name: 'TextCNN',
+        train: '98%',
+        test: '76%',
+        val: '35%',
+        time: '4.5h'
+      }, {
+        ID: '10005',
+        name: 'RNN Attention',
+        train: '98%',
+        test: '89%',
+        val: '63%',
+        time: '2.5h'
+      }, {
+        ID: '10006',
+        name: '综合模型超参',
+        train: '91%',
+        test: '77%',
+        val: '41%',
+        time: '4.5h'
+      }, {
+        ID: '10007',
+        name: '正式上线前配置测试',
+        train: '98%',
+        test: '83%',
+        val: '44%',
+        time: '1h'
+      }, {
+        ID: '10008',
+        name: '正式环境配置',
+        train: '99%',
+        test: '87%',
+        val: '68%',
+        time: '2.5h'
+      }]
+    }
+  }
 }
 </script>
